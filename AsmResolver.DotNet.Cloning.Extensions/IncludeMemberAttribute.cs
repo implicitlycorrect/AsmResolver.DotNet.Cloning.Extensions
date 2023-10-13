2e27f5d261186bbb4a517ae5a543c8d54557f83f
implicitlycorrect <121817193+implicitlycorrect@users.noreply.github.com>
2023-10-13 18:30:56 +0200

    Update IncludeMemberAttribute.cs

--- AsmResolver.DotNet.Cloning.Extensions/IncludeMemberAttribute.cs
+++ AsmResolver.DotNet.Cloning.Extensions/IncludeMemberAttribute.cs
@@ -1,6 +1,11 @@
 ﻿namespace AsmResolver.DotNet.Cloning.Extensions;
 
-public class IncludeMemberAttribute
+/// <summary>
+///     Indicates that a member should be included when cloning types based on the presence of this attribute.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Event | AttributeTargets.Field |
+                AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Property |
+                AttributeTargets.Struct)]
+public sealed class IncludeMemberAttribute : Attribute
 {
-    
 }
