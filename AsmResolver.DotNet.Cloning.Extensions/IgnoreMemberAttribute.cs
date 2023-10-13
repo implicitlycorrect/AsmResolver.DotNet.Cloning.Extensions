93498f8c8ceab625ba61961d5f85d81e8ace4fd1
implicitlycorrect <121817193+implicitlycorrect@users.noreply.github.com>
2023-10-13 18:30:40 +0200

    Update IgnoreMemberAttribute.cs

--- AsmResolver.DotNet.Cloning.Extensions/IgnoreMemberAttribute.cs
+++ AsmResolver.DotNet.Cloning.Extensions/IgnoreMemberAttribute.cs
@@ -1,6 +1,11 @@
 ﻿namespace AsmResolver.DotNet.Cloning.Extensions;
 
-public class IgnoreMemberAttribute
+/// <summary>
+///     Indicates that a member should not be included when cloning types based on the presence of this attribute.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Event | AttributeTargets.Field |
+                AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Property |
+                AttributeTargets.Struct)]
+public sealed class IgnoreMemberAttribute : Attribute
 {
-    
 }
